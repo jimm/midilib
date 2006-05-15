1733499eea5d4aeb8413e5f2fb4d0f029002c6b8
jimmsvn <jimmsvn@f363b343-a9fb-0310-99ac-80d2e3e12c54>
2006-05-15 12:59:52 +0000

    added test/test.mid
    git-svn-id: http://jimm.textdriven.com/svn/midilib/trunk@2 f363b343-a9fb-0310-99ac-80d2e3e12c54

--- lib/midilib/info.rb
+++ lib/midilib/info.rb
@@ -2,8 +2,8 @@ module MIDI
 
     VERSION_MAJOR = 0
     VERSION_MINOR = 8
-    VERSION_TWEAK = 5
+    VERSION_TWEAK = 6
     Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
-    Copyright = 'Copyright (c) 2003-2005 by Jim Menard <jimm@io.com>'
+    Copyright = 'Copyright (c) 2003-2006 by Jim Menard <jimm@io.com>'
 
 end
