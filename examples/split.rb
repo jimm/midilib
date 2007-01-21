9991054c16d1240210e08b90e5886b16f8b1b923
jimmsvn <jimmsvn@f363b343-a9fb-0310-99ac-80d2e3e12c54>
2007-01-21 22:19:12 +0000

    option to extract tempo track separately
    git-svn-id: http://jimm.textdriven.com/svn/midilib/trunk@8 f363b343-a9fb-0310-99ac-80d2e3e12c54

--- examples/split.rb
+++ examples/split.rb
@@ -1,10 +1,13 @@
 #! /usr/bin/env ruby
 #
-# usage: split.rb [midi_file]
+# usage: split.rb [-x] [midi_file]
 #
 # This script splits a MIDI file into muliple files, one for each track. The
 # output files are named with the track's names. Each file contains a copy of
-# the 0'th track, which contains tempo information.
+# the 0th track, which contains tempo information.
+#
+# If -x is specified, the 0th temp track is not included in each file.
+# Instead, it is output in a separate file named 'tempo_track.mid'.
 
 # Start looking for MIDI module classes in the directory above this one.
 # This forces us to use the local copy, even if there is a previously
@@ -15,10 +18,18 @@ require 'midilib/sequence'
 
 DEFAULT_MIDI_TEST_FILE = 'NoFences.mid'
 
+# Command line argument processing
+filename = ARGV[0]
+include_tempo_track = true
+if filename == '-x'
+    include_tempo_track = false
+    filename = ARGV[1]
+end
+
 # Read from MIDI file
 seq = MIDI::Sequence.new()
 
-File.open(ARGV[0] || DEFAULT_MIDI_TEST_FILE, 'rb') { | file |
+File.open(filename || DEFAULT_MIDI_TEST_FILE, 'rb') { | file |
     # The block we pass in to Sequence.read is called at the end of every
     # track read. It is optional, but is useful for progress reports.
     seq.read(file) { | track, num_tracks, i |
@@ -27,10 +38,15 @@ File.open(ARGV[0] || DEFAULT_MIDI_TEST_FILE, 'rb') { | file |
 }
 
 t0 = seq.tracks[0]
+unless include_tempo_track
+    s = MIDI::Sequence.new
+    s.tracks << t0
+    File.open("tempo_track.mid", 'wb') { | file | s.write(file) }
+end
 seq.each_with_index { | track, i |
     next unless i > 0
     s = MIDI::Sequence.new
-    s.tracks << t0
+    s.tracks << t0 if include_tempo_track
     s.tracks << track
     File.open("#{track.name}.mid", 'wb') { | file | s.write(file) }
 }
