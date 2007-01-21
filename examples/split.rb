84d0f135dba485be200016b337da4e729f3be54b
jimmsvn <jimmsvn@f363b343-a9fb-0310-99ac-80d2e3e12c54>
2007-01-21 22:02:16 +0000

    added split.rb, made all .rb file executable
    git-svn-id: http://jimm.textdriven.com/svn/midilib/trunk@7 f363b343-a9fb-0310-99ac-80d2e3e12c54

--- examples/split.rb
+++ examples/split.rb
@@ -1,9 +1,10 @@
 #! /usr/bin/env ruby
 #
-# usage: strings.rb [midi_file]
+# usage: split.rb [midi_file]
 #
-# Prints all strings (track names, etc.) found in the MIDI file.
-#
+# This script splits a MIDI file into muliple files, one for each track. The
+# output files are named with the track's names. Each file contains a copy of
+# the 0'th track, which contains tempo information.
 
 # Start looking for MIDI module classes in the directory above this one.
 # This forces us to use the local copy, even if there is a previously
@@ -11,11 +12,12 @@
 $LOAD_PATH[0, 0] = File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'midilib/sequence'
-require 'midilib/consts'
 
 DEFAULT_MIDI_TEST_FILE = 'NoFences.mid'
 
+# Read from MIDI file
 seq = MIDI::Sequence.new()
+
 File.open(ARGV[0] || DEFAULT_MIDI_TEST_FILE, 'rb') { | file |
     # The block we pass in to Sequence.read is called at the end of every
     # track read. It is optional, but is useful for progress reports.
@@ -24,11 +26,11 @@ File.open(ARGV[0] || DEFAULT_MIDI_TEST_FILE, 'rb') { | file |
     }
 }
 
-include MIDI
-seq.each { | track |
-    track.each { | event |
-	puts event.data if event.meta? &&
-	    [META_TEXT, META_COPYRIGHT, META_SEQ_NAME, META_INSTRUMENT,
-	     META_LYRIC, META_CUE, META_MARKER].include?(event.meta_type)
-    }
+t0 = seq.tracks[0]
+seq.each_with_index { | track, i |
+    next unless i > 0
+    s = MIDI::Sequence.new
+    s.tracks << t0
+    s.tracks << track
+    File.open("#{track.name}.mid", 'wb') { | file | s.write(file) }
 }
