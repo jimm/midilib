f59dbc9d0d2bf6b134680d5f28e8f207ec1c3df8
Jim Menard <jimm@io.com>
2009-01-01 09:25:02 -0500

    Updated copyright date. Bumped tweak version number.

--- lib/midilib/info.rb
+++ lib/midilib/info.rb
@@ -2,8 +2,8 @@ module MIDI
 
     VERSION_MAJOR = 1
     VERSION_MINOR = 1
-    VERSION_TWEAK = 4
+    VERSION_TWEAK = 5
     Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
-    Copyright = 'Copyright (c) 2003-2008 by Jim Menard <jimm@io.com>'
+    Copyright = 'Copyright (c) 2003-2009 by Jim Menard <jimm@io.com>'
 
 end
