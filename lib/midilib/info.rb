1fbdc9756574ad186ab2b5f1b596d64c8ad5408c
Jim Menard <jimm@io.com>
2009-05-06 07:15:10 -0400

    bumped version tweak number

--- lib/midilib/info.rb
+++ lib/midilib/info.rb
@@ -2,7 +2,7 @@ module MIDI
 
     VERSION_MAJOR = 1
     VERSION_MINOR = 2
-    VERSION_TWEAK = 0
+    VERSION_TWEAK = 1
     Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
     Copyright = 'Copyright (c) 2003-2009 by Jim Menard <jimm@io.com>'
 
