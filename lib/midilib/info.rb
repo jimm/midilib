4b7f134f6fc0ca46c9de3c6ace42702c37532504
Jim Menard <jimm@io.com>
2010-04-02 14:55:53 -0400

    Since the interface has changed, bump the major rev number.

--- lib/midilib/info.rb
+++ lib/midilib/info.rb
@@ -1,7 +1,7 @@
 module MIDI
 
-    VERSION_MAJOR = 1
-    VERSION_MINOR = 3
+    VERSION_MAJOR = 2
+    VERSION_MINOR = 0
     VERSION_TWEAK = 0
     Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
     Copyright = 'Copyright (c) 2003-2010 by Jim Menard <jimm@io.com>'
