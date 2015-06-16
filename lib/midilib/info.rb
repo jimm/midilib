c828819be5226afcb31cef76a64d439118532748
Jim Menard <jim@jimmenard.com>
2015-06-16 09:08:56 -0400

    Bumped version number.

--- lib/midilib/info.rb
+++ lib/midilib/info.rb
@@ -2,7 +2,7 @@ module MIDI
 
   VERSION_MAJOR = 2
   VERSION_MINOR = 0
-  VERSION_TWEAK = 4
+  VERSION_TWEAK = 5
   Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
   Copyright = 'Copyright (c) 2003-2015 by Jim Menard <jim@jimmenard.com>'
 
