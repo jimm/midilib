e6787aac44e42a90d02caf71c8eec0fd5cbf2699
Jim Menard <jim@jimmenard.com>
2022-04-30 10:17:42 -0400

    fixed CC numbers for CC_GEN_PURPOSE_{5,6,7,8}

--- lib/midilib/info.rb
+++ lib/midilib/info.rb
@@ -1,7 +1,7 @@
 module MIDI
   VERSION_MAJOR = 3
   VERSION_MINOR = 0
-  VERSION_TWEAK = 0
+  VERSION_TWEAK = 1
   Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
-  Copyright = 'Copyright (c) 2003-2021 by Jim Menard <jim@jimmenard.com>'
+  Copyright = 'Copyright (c) 2003-2022 by Jim Menard <jim@jimmenard.com>'
 end
